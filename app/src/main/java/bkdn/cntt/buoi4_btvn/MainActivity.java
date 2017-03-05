8c1fed9dcacea29bb2a3bacb0964f26c25611996
Administrator <nttinh995@gmail.com>
2017-03-05 23:25:24 +0700

    add code

---
+++ app/src/main/java/bkdn/cntt/buoi4_btvn/MainActivity.java
@@ -0,0 +1,14 @@
+package bkdn.cntt.buoi4_btvn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fbkhac);
+        setContentView(R.layout.sign_up);
+    }
+}
